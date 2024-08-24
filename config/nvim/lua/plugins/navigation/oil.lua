46794fc3165b816ac593314ea3485194249a0b4c
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-08-24 15:53:53 +0100

    add oil

---
+++ config/nvim/lua/plugins/navigation/oil.lua
@@ -0,0 +1,12 @@
+return {
+	"stevearc/oil.nvim",
+	opts = {},
+	keys = {
+		{
+			"-",
+			"<CMD>Oil<CR>",
+			"Oil: open parent directory",
+		},
+	},
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+}
