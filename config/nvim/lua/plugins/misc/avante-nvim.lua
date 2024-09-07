149416db43d248496cb86399003f08f87e58ff30
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-09-07 21:59:13 +0100

    .

--- config/nvim/lua/plugins/misc/avante-nvim.lua
+++ config/nvim/lua/plugins/misc/avante-nvim.lua
@@ -3,37 +3,41 @@ return {
 	event = "VeryLazy",
 	lazy = false,
 	opts = {
-		-- add any opts here
+		hints = { enabled = false },
+		provider = "claude",
+		claude = {
+			api_key_name = "cmd:cat /Users/mous/.anthropic",
+		},
+		windows = {
+			position = "right",
+			wrap = true,
+			width = 40,
+			sidebar_header = {
+				align = "right",
+				rounded = false,
+			},
+		},
 	},
-	-- if you want to download pre-built binary, then pass source=false. Make sure to follow instruction above.
-	-- Also note that downloading prebuilt binary is a lot faster comparing to compiling from source.
 	build = ":AvanteBuild source=false",
 	dependencies = {
 		"stevearc/dressing.nvim",
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
-		--- The below dependencies are optional,
-		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-		-- "zbirenbaum/copilot.lua", -- for providers='copilot'
+		"nvim-tree/nvim-web-devicons",
 		{
-			-- support for image pasting
 			"HakonHarnes/img-clip.nvim",
 			event = "VeryLazy",
 			opts = {
-				-- recommended settings
 				default = {
 					embed_image_as_base64 = false,
 					prompt_for_file_name = false,
 					drag_and_drop = {
 						insert_mode = true,
 					},
-					-- required for Windows users
-					use_absolute_path = true,
 				},
 			},
 		},
 		{
-			-- Make sure to set this up properly if you have lazy=true
 			"MeanderingProgrammer/render-markdown.nvim",
 			opts = {
 				file_types = { "markdown", "Avante" },
