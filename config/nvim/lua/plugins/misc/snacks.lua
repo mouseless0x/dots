8d07532c40db2eabd82fa5cdcfe5eb7e06acf6d7
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-11-25 07:52:30 +0000

    snacks :)

---
+++ config/nvim/lua/plugins/misc/snacks.lua
@@ -0,0 +1,13 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		notifier = { enabled = true },
+		dashboard = { enabled = true },
+		quickfile = { enabled = true },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+	},
+}
