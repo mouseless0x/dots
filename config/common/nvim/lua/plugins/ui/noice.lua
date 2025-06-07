71304f1eb14516258378308e1c3b334d7900b3b4
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2025-06-07 02:26:56 +0100

    setup noice style

--- config/common/nvim/lua/plugins/ui/noice.lua
+++ config/common/nvim/lua/plugins/ui/noice.lua
@@ -1,15 +1,32 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-        cmdline = {
-            enabled = false,
-        },
-        notify = {
-            enabled = false,
-        },
-    },
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-    },
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		--cmdline = {
+		--    enabled = false,
+		--},
+		notify = {
+			enabled = false,
+		},
+		presets = {
+			lsp_doc_border = true, -- add a border to hover docs and signature help
+		},
+		views = {
+			hover = {
+				border = {
+					style = "rounded",
+				},
+				win_options = {
+					winhighlight = {
+						Normal = "Normal",
+						FloatBorder = "Normal",
+					},
+				},
+			},
+		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
 }
+
