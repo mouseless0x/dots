80b31d9c52d34c9d7fcadc064f299717dc409710
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2025-06-07 14:31:33 +0100

    setup noice

--- config/common/nvim/lua/plugins/ui/noice.lua
+++ config/common/nvim/lua/plugins/ui/noice.lua
@@ -5,14 +5,40 @@ return {
 		--cmdline = {
 		--    enabled = false,
 		--},
+		lsp = {
+			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+			},
+		},
 		notify = {
 			enabled = false,
 		},
 		presets = {
+			bottom_search = true,
 			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
+		cmdline = {
+			enabled = true,
+			view = "cmdline",
+		},
+		override = {
+			-- override the default lsp markdown formatter with Noice
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			-- override the lsp markdown formatter with Noice
+			["vim.lsp.util.stylize_markdown"] = true,
+			-- override cmp documentation with Noice (needs the other options to work)
+			["cmp.entry.get_documentation"] = true,
+		},
 		views = {
+			signature = {
+				scrollbar = true,
+				size = { width = "auto", height = "auto" },
+			},
 			hover = {
+				scrollbar = false,
 				border = {
 					style = "rounded",
 				},
@@ -29,4 +55,3 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 }
-
