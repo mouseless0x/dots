120ae0713ce0ace7ce53ea2b61f1d22017fdbc56
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2025-06-18 11:50:31 +0100

    chore(plugins): remove gitlinker plugin configuration

--- config/common/nvim/lua/plugins/ui/noice.lua
+++ config/common/nvim/lua/plugins/ui/noice.lua
@@ -5,14 +5,6 @@ return {
 		--cmdline = {
 		--    enabled = false,
 		--},
-		lsp = {
-			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-			},
-		},
 		notify = {
 			enabled = false,
 		},
@@ -23,14 +15,6 @@ return {
 		cmdline = {
 			enabled = true,
 			view = "cmdline",
-		},
-		override = {
-			-- override the default lsp markdown formatter with Noice
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			-- override the lsp markdown formatter with Noice
-			["vim.lsp.util.stylize_markdown"] = true,
-			-- override cmp documentation with Noice (needs the other options to work)
-			["cmp.entry.get_documentation"] = true,
 		},
 		views = {
 			signature = {
